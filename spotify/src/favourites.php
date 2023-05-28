68188576fe299af8aa9f6a4488fadb3d77aaaac4
Asfin Rizaldy <90314986+ANRY17@users.noreply.github.com>
2023-05-28 10:31:19 +0700

    Update favourites.php

--- spotify/src/favourites.php
+++ spotify/src/favourites.php
@@ -1,4 +1,18 @@
-<?php include("./index.php") ?>
+<?php include("./index.php") 
+function addFavorite($song_id, $user_id) {
+  global $db;
+
+  $stmt = $db->prepare('INSERT INTO favorites (song_id, user_id) VALUES (:song_id, :user_id)');
+  $stmt->bindParam(':song_id', $song_id);
+  $stmt->bindParam(':user_id', $user_id);
+
+  if ($stmt->execute()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+?>
 <script>
     if (!authenticated) {
         loginPopup();
